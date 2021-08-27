00a18d7dfa97129d8e62e1cd6b82fe905d05ec07
Alex <a.a.alexashkin@mail.ru>
2021-08-27 16:53:38 +0300

    Lesson_2_Tasks

--- geekSH/geekshop/products/views.py
+++ geekSH/geekshop/products/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-
+import datetime
+import json
+from geekshop.settings import STATIC_URL
 # Create your views here.
 
 
 def index(request):
-	return render(request, "index.html")
+	content = {"date": datetime.datetime.now().strftime("%d %B %Y")}
+	return render(request, "index.html", content)
 
 def products(request):
-	return render(request, "products.html")
+	content = {"date": datetime.datetime.now().strftime("%d %B %Y")}
+
+	with open("products/data.json", "r", encoding="utf-8") as file:
+		content.update({"goods": json.load(file)})
+	for i in content["goods"]:
+		i["url"] = STATIC_URL + i["url"]
+	return render(request, "products.html", content)
