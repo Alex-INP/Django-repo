20338188877c7ac60cc6d8302a79c1ee428dd20a
Alex <a.a.alexashkin@mail.ru>
2021-10-11 14:12:21 +0300

    settings

--- geekSH/geekshop/geekshop/settings.py
+++ geekSH/geekshop/geekshop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-r)ci=zg!7&!!^zj=c=3f2!@x4alf_2d5$$$@alsg6rtx3y#6a&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -87,13 +87,20 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geekshop',
+        'USER': 'postgres',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
