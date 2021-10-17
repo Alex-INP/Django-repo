546f6e8d9c8077c3cab4448ccdbea3dd0c4c088a
Alex <a.a.alexashkin@mail.ru>
2021-10-17 17:28:46 +0300

    L_6.2_3

--- geekSH/geekshop/ordersapp/forms.py
+++ geekSH/geekshop/ordersapp/forms.py
@@ -20,7 +20,11 @@ class OrderItemsForm(forms.ModelForm):
     class Meta:
         model = OrderItem
         exclude = ()
-        def __init__(self, *args, **kwargs):
-            super(OrderItemsForm, self).__init__(*args, **kwargs)
-            for field_name, field in self.fields.items():
-                field.widget.attrs['class'] = 'form-control'
+
+    def __init__(self, *args, **kwargs):
+        super(OrderItemsForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+        self.fields["product"].queryset = Product.objects.all().select_related()
