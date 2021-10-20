2a622b161000441d9f74e67d673c048c1ce46f7f
Alex <a.a.alexashkin@mail.ru>
2021-10-20 19:27:25 +0300

    Lesson_7.2

--- geekSH/geekshop/products/views.py
+++ geekSH/geekshop/products/views.py
@@ -2,11 +2,54 @@ from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.core.cache import cache
+from django.shortcuts import get_object_or_404
+from django.views.decorators.cache import cache_page, never_cache
 
 import datetime
 from .models import ProductCategory, Product
+from geekshop import settings
 # Create your views here.
 
+def get_links_category():
+	if settings.LOW_CACHE:
+		key = "links_category"
+		links_category = cache.get(key)
+
+		if links_category is None:
+			links_category = ProductCategory.objects.filter(is_active=True).select_related()
+			cache.set(key, links_category)
+		return links_category
+	else:
+		return ProductCategory.objects.filter(is_active=True)
+
+def get_links_product():
+	if settings.LOW_CACHE:
+		key = "links_product"
+		links_product = cache.get(key)
+
+		if links_product is None:
+			links_product = Product.objects.filter(is_active=True).select_related()
+			cache.set(key, links_product)
+		return links_product
+	else:
+		return Product.objects.filter(is_active=True)
+
+def get_product(pk):
+	if settings.LOW_CACHE:
+		key = f"product{pk}"
+		product = cache.get(key)
+
+		if product is None:
+			product = get_object_or_404(Product, pk=pk)
+			cache.set(key, product)
+		return product
+	else:
+		return get_object_or_404(Product, pk=pk)
+
+
+# @never_cache
+# @cache_page(3600)
 
 def index(request):
 	content = {"date": datetime.datetime.now().strftime("%d %B %Y")}
@@ -24,8 +67,12 @@ class ProductsShow(ListView):
 			self.object_list = Product.objects.filter(category=id).select_related("category")
 		else:
 			self.object_list = Product.objects.all().select_related("category")
+
+		if not settings.LOCAL_HOST_DEVELOP:
+			self.object_list = get_links_product()
+
 		context = super(ProductsShow, self).get_context_data(**kwargs)
-		context["categories"] = ProductCategory.objects.all()
+		context["categories"] = get_links_category()
 		return context
 
 
@@ -33,6 +80,11 @@ class DetailProduct(DetailView):
 	context_object_name = "product"
 	template_name = "products/detail-product.html"
 	model = Product
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context["product"] = get_product(self.kwargs.get("pk"))
+		return context
 
 
 # ---------------------------------------Old FBV---------------------------------------
